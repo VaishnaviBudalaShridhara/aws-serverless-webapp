e64f30c78f188b5a8a133f80f1d40f5c8315ec88
Vaishnavi Budala Shridhara <161613121+VaishnaviBudalaShridhara@users.noreply.github.com>
2025-05-19 10:43:15 +0200

    Update index.js

--- lambda/index.js
+++ lambda/index.js
@@ -1,1 +1,21 @@
+const AWS = require("aws-sdk");
+const db = new AWS.DynamoDB.DocumentClient();
+const tableName = process.env.TABLE_NAME || "Tasks";
 
+exports.handler = async (event) => {
+  const body = JSON.parse(event.body);
+  const taskId = Date.now().toString();
+
+  await db.put({
+    TableName: tableName,
+    Item: {
+      taskId,
+      task: body.task
+    }
+  }).promise();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Task saved", taskId }),
+  };
+};
