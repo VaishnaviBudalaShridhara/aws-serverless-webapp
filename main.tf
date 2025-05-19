1a76927608ab605900c60bd7b4eacd53ddb644f5
Vaishnavi Budala Shridhara <161613121+VaishnaviBudalaShridhara@users.noreply.github.com>
2025-05-19 10:41:54 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,98 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "static_site" {
+  bucket = "my-serverless-app-site"
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "allow_public_read" {
+  bucket = aws_s3_bucket.static_site.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = "*",
+      Action   = ["s3:GetObject"],
+      Resource = "${aws_s3_bucket.static_site.arn}/*"
+    }]
+  })
+}
+
+resource "aws_dynamodb_table" "tasks" {
+  name         = "Tasks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "taskId"
+
+  attribute {
+    name = "taskId"
+    type = "S"
+  }
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action    = "sts:AssumeRole",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+      Effect = "Allow",
+      Sid     = ""
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_dynamo_policy" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_exec" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "api_handler" {
+  filename         = "lambda_function_payload.zip"
+  function_name    = "taskHandler"
+  role             = aws_iam_role.lambda_exec.arn
+  handler          = "index.handler"
+  runtime          = "nodejs18.x"
+  source_code_hash = filebase64sha256("lambda_function_payload.zip")
+}
+
+resource "aws_apigatewayv2_api" "http_api" {
+  name          = "TaskAPI"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id             = aws_apigatewayv2_api.http_api.id
+  integration_type   = "AWS_PROXY"
+  integration_uri    = aws_lambda_function.api_handler.invoke_arn
+  integration_method = "POST"
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "default" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "POST /tasks"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "dev" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "dev"
+  auto_deploy = true
+}
 
